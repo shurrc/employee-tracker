7c535bf0e3eae24bc7e753026713f8364cf505ef
Kathy Shurr <kathyshurr@kathys-mbp.myfiosgateway.com>
2022-11-05 20:05:36 -0400

    built out functions for add manager, add employee add role, changed allEmployee so that it includes data from other tables

--- helper/mysql.js
+++ helper/mysql.js
@@ -6,10 +6,23 @@ function findAll(table) {
     
 }
 
+function findAllEmployees(employees) {
+    return db.promise().query(`SELECT *
+    FROM ${employees}
+    LEFT JOIN roles ON employees.roles_id = roles.id
+    LEFT JOIN departments ON roles.departments_id = departments_id;`)
+}
+
 function addNewDepartment(department) {
     return db.promise().query('INSERT INTO departments (department_name) VALUES (?);', department)
 }
 
+function addNewRole(role) {
+    return db.promise().query('INSERT INTO roles (title, salary, departments_id) VALUES (?, ?, ?);', role)
+}
 
+function addNewEmployee(employee) {
+    return db.promise().query('INSERT INTO employees (first_name, last_name, roles_id, manager_id) VALUES (?, ?, ?, ?);', employee)
+}
 
-module.exports = {findAll, addNewDepartment};
+module.exports = {findAll, addNewDepartment,findAllEmployees, addNewRole, addNewEmployee};
--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-//Dependencies
 const promptList = require("./helper/inquirer-list")
 const promptInput = require("./helper/inquirer-prompt");
-const { findAll, addNewDepartment } = require("./helper/mysql");
+const { findAll, addNewDepartment, addNewRole, addNewEmployee, findAllEmployees} = require("./helper/mysql");
 require("console.table");
-//inquirer
-//mysql2
 
 
-
-
-
-
-//Data
-
-
-
-
-//Functions
 async function start() {
-    const todo = await promptList("What would you like to do",["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]);
+    const todo = await promptList("What would you like to do",["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add a manager", "Add an employee", "Update an employee role"]);
     console.log(todo);
     switch(todo) {
         case "View all departments":
@@ -35,6 +22,9 @@ async function start() {
             break;
         case 'Add a role':
             addRole()
+            break;
+        case 'Add a manager':
+            addManager()
             break;
         case 'Add an employee':
             addEmployee()
@@ -63,7 +53,7 @@ function allRoles() {
 };
 
 function allEmployees() {
-    findAll('employees').then(([data]) => {
+    findAllEmployees('employees').then(([data]) => {
         console.table(data)
         setTimeout(() => start(), 3000);
     }) 
@@ -78,7 +68,58 @@ async function addDepartment() {
 
 }
 
+async function addRole() {
+    const roleName = await promptInput("What is the name of the role you would like to add?");
+    let salary = await promptInput("What is the salary of this position?");
+    salary = parseInt(salary)
+    await findAll('departments').then(([data]) => {
+        console.table(data);
+    });
+    let department = await promptInput("What is the id of the department this role belongs too? Reference the table above");
+    department = parseInt(department);
+    const array = [roleName, salary, department];
+    console.log(array)
+    addNewRole(array).then((data) => {
+        console.log('role added!')
+        setTimeout(() => start(), 3000);
+    })
+}
+
+async function addManager() {
+    const firstName = await promptInput("What is your first name?");
+    const lastName = await promptInput("What is your last name?");
+    await findAll('roles').then(([data]) => {
+        console.table(data);
+    });
+    let role = await promptInput("What is the id of your role? Reference table above");
+    role = parseInt(role);
+    let type = null
+    const array = [firstName, lastName, role, type]
+    addNewEmployee(array).then((data) => {
+        console.log('Manager added!')
+        setTimeout(() => start(), 3000);
+    })
+}
+
+
+async function addEmployee() {
+    const firstName = await promptInput("What is your first name?");
+    const lastName = await promptInput("What is your last name?");
+    await findAll('roles').then(([data]) => {
+        console.table(data);
+    });
+    let role = await promptInput("What is the id of your role? Reference table above");
+    role = parseInt(role);
+    await findAll('employees').then(([data]) => {
+        console.table(data);
+    });
+    let manager = await promptInput("What is the id of your manager? Reference the table above");
+    manager = parseInt(manager);
+    const array = [firstName, lastName, role, manager];
+    addNewEmployee(array).then((data) => {
+        console.log('Employee added!')
+        setTimeout(() => start(), 3000);
+    })
+}
+
 start()
-
-
-//initiallization
